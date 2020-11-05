ff5d7c411e04271b4ea91fed5a3d6515a63d6f6d
vbrignatz <vbrignatz@aphrodite.univ-avignon.fr>
2020-11-05 11:37:58 +0100

    Added multiple extraction format Updated readme

--- extract.py
+++ extract.py
@@ -16,27 +16,15 @@ from parser import fetch_config
 from models import resnet34
 from cuda_test import cuda_test, get_device
 
-
-def extract(gen, ds, device, path):
-    gen.eval()
-
-    ark_scp_xvector = f'ark:| copy-vector ark:- ark,scp:{path}/xvector.ark,{path}/xvector.scp'
-    with kaldi_io.open_or_fd(ark_scp_xvector,'wb') as f:
-        with torch.no_grad():
-            for i in tqdm(range(len(ds))):
-                feats, _, utt = ds.__getitem__(i)
-                feats = feats.unsqueeze(0).unsqueeze(1).to(device)
-                embeds = gen(feats).cpu().numpy()
-                kaldi_io.write_vec_flt(f, embeds[0], key=utt)
-
-
 if __name__ == "__main__":
+    # Arguments parsing
     parser = argparse.ArgumentParser(description='Extract the xvectors of a dataset given a model. (xvectors will be extracted in <model_dir>/xvectors/<checkpoint>/)')
 
     parser.add_argument("-m", "--modeldir", type=str, required=True, help="The path to the model directory you want to extract the xvectors with. This dir should containt the file experiement.cfg")
     parser.add_argument('--checkpoint', '--resume-checkpoint', type=int, default=-1,
                             help="Model Checkpoint to use. Default (-1) : use the last one ")
     parser.add_argument("-d", '--data', type=str, required=True, help="Path to the kaldi data folder to extract (Should contain spk2utt, utt2spk and feats.scp).")
+    parser.add_argument("-f", "--format", type=str, choices=["ark", "txt", "pt"], default="ark", help="The output format you want the x-vectors in.")
 
     args = parser.parse_args()
     args.modeldir = Path(args.modeldir)
@@ -47,6 +35,7 @@ if __name__ == "__main__":
 
     args = fetch_config(args) 
 
+    # Find the right data path
     try:
         data_path = {"train":args.train_data_path,
                      "eval" :args.eval_data_path,
@@ -84,6 +73,32 @@ if __name__ == "__main__":
     generator = resnet34(args)
     generator.load_state_dict(torch.load(g_path), strict=False)
     generator = generator.to(device)
+    generator.eval()
 
+    # Extract xv
+    if args.format in ["ark", "txt"]:
+        if args.format == "ark":
+            ark_scp_xvector = f'ark:| copy-vector ark:- ark,scp:{out_dir}/xvectors.ark,{out_dir}/xvectors.scp'
+            mode = "wb"
+        if args.format == "txt":
+            ark_scp_xvector = f'ark:| copy-vector ark:- ark,t:{out_dir}/xvectors.txt'
+            mode = "w"
+
+        with kaldi_io.open_or_fd(ark_scp_xvector, mode) as f:
+            with torch.no_grad():
+                for i in tqdm(range(len(ds_extract))):
+                    feats, _, utt = ds_extract.__getitem__(i)
+                    feats = feats.unsqueeze(0).unsqueeze(1).to(device)
+                    embeds = generator(feats).cpu().numpy()
+                    kaldi_io.write_vec_flt(f, embeds[0], key=utt)
+
+    if args.format == "pt":
+        out_dir = out_dir / "xvectors_pt/"
+        out_dir.mkdir(parents=True, exist_ok=True)
+        with torch.no_grad():
+            for i in tqdm(range(len(ds_extract))):
+                feats, _, utt = ds_extract.__getitem__(i)
+                feats = feats.unsqueeze(0).unsqueeze(1).to(device)
+                embeds = generator(feats).cpu().numpy()
+                torch.save(embeds[0], out_dir / f"{utt}.pt")
     
-    extract(generator, ds_extract, device, out_dir)
